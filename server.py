9561f8259d5b7a89600cbd008c1e8da0f459a11b
Fiona8199 <1658310695@qq.com>
2025-06-25 02:19:15 +0800

    change2
    Processing requests

--- server.py
+++ server.py
@@ -15,4 +15,37 @@ stats = {
     'total_gets': 0,
     'total_puts': 0,
     'errors': 0
-}
+}def process_request(client_socket, request):
+    global stats
+    command = request[1]
+    key = request[3:].split()[0]
+    value = request[3:].split()[1] if command == 'P' else None
+
+    if command == 'P':
+        if key in tuple_space:
+            client_socket.sendall(f"{len('ERR k already exists')} ERR k already exists".encode())
+            stats['errors'] += 1
+        else:
+            tuple_space[key] = value
+            stats['tuples_count'] += 1
+            stats['total_operations'] += 1
+            stats['total_puts'] += 1
+            client_socket.sendall(f"{len(f'OK (k, v) added')} OK (k, v) added".encode())
+    elif command == 'R':
+        if key in tuple_space:
+            stats['total_operations'] += 1
+            stats['total_reads'] += 1
+            client_socket.sendall(f"{len(f'OK (k, v) read {tuple_space[key]}')} OK (k, v) read {tuple_space[key]}".encode())
+        else:
+            stats['errors'] += 1
+            client_socket.sendall(f"{len('ERR k does not exist')} ERR k does not exist".encode())
+    elif command == 'G':
+        if key in tuple_space:
+            value = tuple_space.pop(key)
+            stats['tuples_count'] -= 1
+            stats['total_operations'] += 1
+            stats['total_gets'] += 1
+            client_socket.sendall(f"{len(f'OK (k, v) removed {value}')} OK (k, v) removed {value}".encode())
+        else:
+            stats['errors'] += 1
+            client_socket.sendall(f"{len('ERR k does not exist')} ERR k does not exist".encode())
